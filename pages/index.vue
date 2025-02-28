51c3f9446d98db577a78bafc58436ceb0198c94d
Nathan <nathan@hprl.dev>
2025-02-27 21:09:40 -0500

    chore: update meta

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 useSeoMeta({
   title: 'Home',
   ogTitle: 'Home',
-  description: 'Nuxt 3 Starter, with TypeScript, Tailwind 4, and Supabase.',
-  ogDescription: 'Nuxt 3 Starter, with TypeScript, Tailwind 4, and Supabase.',
+  description: 'Nuxt 3 Template, with TypeScript, Tailwind 4, DrizzleORM and Supabase.',
+  ogDescription: 'Nuxt 3 Template, with TypeScript, Tailwind 4, DrizzleORM and Supabase.',
   twitterCard: 'summary_large_image',
 })
 </script>
