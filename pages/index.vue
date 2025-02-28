f9911d3c66df5d70c368f8a864ae1d2cde78437a
Nathan <nathan@hprl.dev>
2025-02-27 21:02:42 -0500

    chore: add useSeoMeta

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Home',
+  ogTitle: 'Home',
+  description: 'Nuxt 3 Starter, with TypeScript, Tailwind 4, and Supabase.',
+  ogDescription: 'Nuxt 3 Starter, with TypeScript, Tailwind 4, and Supabase.',
+  twitterCard: 'summary_large_image',
+})
+</script>
 
 <template>
   <div class="container mx-auto p-4">
