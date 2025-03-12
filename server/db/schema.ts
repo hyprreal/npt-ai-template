2325692352a841d73684c6aab3c8cb4284c1fa49
Nathan <nathan@hprl.dev>
2025-03-12 03:43:55 -0400

    schema: remove profile

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,4 +1,3 @@
-import { sql } from 'drizzle-orm'
 import {
   boolean,
   check,
@@ -8,22 +7,6 @@ import {
   timestamp,
   uuid,
 } from 'drizzle-orm/pg-core'
-
-export const profile = pgTable('profile', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  created_at: timestamp('created_at').defaultNow(),
-  updated_at: timestamp('updated_at').defaultNow(),
-  email: text('email').notNull().unique(),
-  username: text('username').notNull().unique(),
-  phone_number: text('phone_number').notNull().unique(),
-  first_name: text('first_name').notNull(),
-  last_name: text('last_name').notNull(),
-  date_of_birth: text('date_of_birth').notNull(),
-  is_member: boolean('is_member').default(false),
-}, table => [
-  index('profile_email_idx').on(table.email),
-  check('valid_username', sql`${table.username} ~* '^[a-zA-Z0-9]+$'`),
-])
 
 export const item = pgTable('item', {
   id: uuid('id').primaryKey().defaultRandom(),
