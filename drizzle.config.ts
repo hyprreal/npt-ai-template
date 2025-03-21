793570e6682ccd66ce3649770e4eb1086d53baf3
Nathan <nathan@hprl.dev>
2025-03-20 22:36:45 -0400

    chore: update schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './supabase/schema/schema.ts',
+  schema: './supabase/schema/schema',
   out: './supabase/migrations',
   dbCredentials: {
     url: process.env.LOCAL_DATABASE_URL as string,
