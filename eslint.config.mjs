bcc9b4be9dcdb38004f4e5ac03a1838e1a852688
Nathan <nathan@hprl.dev>
2025-05-09 09:55:05 -0400

    refactor: add antfu config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,27 @@
+import antfu from '@antfu/eslint-config'
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  {
-    rules: {
-      'vue/html-self-closing': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
+// TODO: add eslint-plugin-tailwindcss
+
+export default withNuxt(antfu({
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: false,
+    quotes: 'single',
   },
-)
+  rules: {
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+    'ts/no-redeclare': 'off',
+  },
+}))
