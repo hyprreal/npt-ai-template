411106d8d94ec42f5b444ccd39a45aa34dddfe38
Nathan <nathan@hprl.dev>
2025-05-09 09:59:40 -0400

    refactor: use defineValidatedEventHandler

--- server/api/item/index.post.ts
+++ server/api/item/index.post.ts
@@ -1,10 +1,17 @@
-// import { serverSupabaseUser } from '#supabase/server'
-import { item } from '~/supabase/schema'
+import { z } from 'zod'
+import { item } from '~/db/schema'
 import { db } from '~/server/utils/db-connection'
 import { handleDatabaseError } from '~/server/utils/db-error-handler'
+import defineValidatedEventHandler from '~/server/utils/define-validated-event-handler'
 
-export default defineEventHandler(async (event) => {
-  const newItem = await readBody(event)
+const itemSchema = z.object({
+  name: z.string().trim().min(1),
+  description: z.string().trim().min(1),
+  is_private: z.boolean(),
+})
+
+export default defineValidatedEventHandler(itemSchema, async (event) => {
+  const { body: newItem } = event.context.validated
 
   try {
     await db
