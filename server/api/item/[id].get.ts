9c472731e178a63f731a1da7c5432ac24334a238
Nathan <nathan@hprl.dev>
2025-07-19 11:16:45 -0400

    refactor:  add param validation

--- server/api/item/[id].get.ts
+++ server/api/item/[id].get.ts
@@ -1,9 +1,16 @@
+import { z } from 'zod'
 import { eq } from 'drizzle-orm'
 import { item } from '~~/db/schema/item'
 import { db } from '~~/server/utils/db-connection'
 
 export default defineEventHandler(async (event) => {
-  const itemId = getRouterParam(event, 'id')
+  const itemId = getRouterParam(event, 'id') as string
+  if (!z.string().safeParse(itemId).success) {
+    throw createError({
+      statusCode: 422,
+      statusMessage: 'Invalid item id',
+    })
+  }
 
   return db
     .select()
