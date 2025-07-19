263b5b337f4052f1c6dd8ca98a6b690bcfb2599e
Nathan <nathan@hprl.dev>
2025-07-19 11:18:37 -0400

    refactor: add session check

--- server/utils/define-validated-event-handler.ts
+++ server/utils/define-validated-event-handler.ts
@@ -1,5 +1,6 @@
 import type { H3Event, H3EventContext } from 'h3'
 import type { z, ZodType } from 'zod'
+import { auth } from '~~/lib/auth'
 
 type ValidatedEvent<S extends ZodType> = H3Event & {
   context: H3EventContext & {
@@ -14,15 +15,36 @@ export default function defineValidatedEventHandler<S extends ZodType>(
   handler: (event: ValidatedEvent<S>) => any,
 ) {
   return defineEventHandler(async (event) => {
+    const session = await auth.api.getSession({
+      headers: event.headers,
+    })
+
+    if (!session || !session.user) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: 'Unauthorized.',
+      })
+    }
+
     const result = await readValidatedBody(event, schema.safeParse)
+
     if (result.error) {
       setResponseStatus(event, 422)
+
+      const statusMessage = result
+        .error
+        .issues
+        .map(issue => `${issue.path.join('')}: ${issue.message}`)
+        .join('; ')
+
       return {
         success: false,
-        errors: result.error,
+        errors: statusMessage,
       }
     }
+
     event.context.validated = {
+      user: session.user,
       body: result.data,
     }
 
